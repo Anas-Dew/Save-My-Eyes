16c49f303f5f6ec1c1185fdfad5c722d9b2a306d
Anas Raza <96536838+Anas-Dew@users.noreply.github.com>
2022-02-17 19:15:38 +0530

    Lag Fixed & Menubar working
    Lag is fixed now and options menu and about menu are working. But their core features aren't working.

--- App/test.py
+++ App/test.py
@@ -1,7 +1,21 @@
-from ctypes import alignment
-from time import sleep
+from threading import Timer
+from tkinter import ttk
 from pygame import mixer
-from tkinter import *
+from tkinter import * 
+import os
+from os import name
+# -----------------Initializing---------------!!!
+def Work():
+  
+    # for windows
+    if name == 'nt':
+        _ = os.chdir('C:/Projects/Save-My-Eyes/App/')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        username = os.getlogin()
+        _ = os.chdir(f'/home/{username}/Projects/Save-My-Eyes/App/')
+Work()
 
 mixer.init()
 root = Tk()
@@ -10,38 +24,24 @@ root.resizable('False','False')
 width,height = 250,190
 root.geometry(f"{width}x{height}")
 
-bottom_plate = Label(text="Eye Care : OFF", bg="Red",
-                     fg="White", font="sans 9 italic")
-bottom_plate.pack(side=BOTTOM, fill=X)
+timer_val = 5 # Timer for each iteration
+timer_repeat = 2 # Total num of iterations
 
-
-
-def relax():
+# -----------------Important Functions---------------!!!
+def library():
         mixer.music.load('Voice1.mp3')
         mixer.music.load('Voice2.mp3')
-        mixer.music.play(1)
+        mixer.music.play()
 
-
-def timer():
-        timer_val = 5
-        for j in range(12):
-
-                
-                for i in range(1200):
-                        sleep(1)
-                        timer_val-=1
-                        
-                        if timer_val == 0:
-                                relax()
         
+def timer():
+        for repeat in range(timer_repeat):
+                t = Timer(timer_val,library)
+                t.start()
+                print("Loop happend")
 
-# timer_display = Label(root,text=,bg="Black",fg="White",cursor="watch")
-# timer_display.pack()
-
-Main_button = Button(root,text='Activate',width=10,command=timer)
-Main_button.pack(pady=60)
-                        
 def text_update(event):
+        
         if bottom_plate['text'] == 'Eye Care : OFF':
 
                 bottom_plate.config(text='Eye Care :  ON')
@@ -51,14 +51,54 @@ def text_update(event):
                 Main_button.config(text='Activated')
         else:
                 Main_button.config(text='Activate')
-                
-               
+# -----------------Other Windows---------------!!!
+
+def options():
+        root = Tk()
+        root.title("Options")
+        root.resizable('False','False')
+        width,height = 230,170
+        root.geometry(f"{width}x{height}")
+
+        Title = Label(root,text="Select From Voices Below",font="sans 10")
+        Title.pack(pady=10)
+
+        options_list = ["Voice 1", "Voice 2", "Voice 3"]
+        value_inside = StringVar(root)
+        value_inside.set(options_list[1])
+
+        def h(event):
+                print(value_inside.get())
+        root.bind('<Return>',h)
+        
+        question_menu = OptionMenu(root, value_inside, *options_list)
+        question_menu.pack(pady=6)
+        
+        root.mainloop()
+
+def about():
+        root = Tk()
+        root.title("About")
+        root.mainloop()
+        print("Working...")
+
+
+# -----------------Menus & Buttons---------------!!!
+Main_button = Button(root,text='Activate',width=10,command=timer)
+Main_button.pack(pady=60)
+
 menubar = Menu(root)
-menubar.add_command(label='Options',command=None)
-menubar.add_command(label='About',command=None)
+menubar.add_command(label='Options',command=options)
+menubar.add_command(label='About',command=about)
+
 root.config(menu=menubar)
 root.bind('<Button-1>',text_update)
 
+bottom_plate = Label(text="Eye Care : OFF", bg="Red",
+                     fg="White", font="sans 9 italic")
+bottom_plate.pack(side=BOTTOM, fill=X)
+    
+# ------------Program-Starts-Here--------------
 if __name__=="__main__":
     
     root.mainloop()
